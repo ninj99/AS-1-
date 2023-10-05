327c5bf7483dad99a361cec995bd9da88c605dbc
Hassan Abdullah <92162768+ninj99@users.noreply.github.com>
2023-10-05 12:14:27 +0500

    mathods

--- Program.cs
+++ Program.cs
@@ -1,79 +1,24 @@
 ﻿using System;
-using System.Data;
-using System.Diagnostics.CodeAnalysis;
 
-class Program
+namespace Lern_BroCode
 {
-  //mathod 1 
-    public static void Add(int a, int b)
+    internal class Program
     {
-        int sum = a + b;
-        Console.WriteLine(sum);
-    }
-
-    
-   
-    
-  
-    
-    //mathod 5 
-    public static void subtract(int a, int b)
-    {
-        int sum = a - b;
-        Console.WriteLine(sum);
-    }
-    
-    //mathod 6 
-    public static void multiply(int a, int b)
-    {
-        int sum = a * b;
-        Console.WriteLine(sum);
-    }
-    
-    //mathod 7
-    public static void divide(int a, int b)
-    {
-        int sum = a / b;
-        Console.WriteLine(sum);
-    }
-    public static void calculator(int a,string sighn,int b)
-    {
-
-        if (sighn == "*")
+        public static void Main(string[] args)
         {
-            multiply(a,b);
-        }
+            double x;
+            Random fir = new Random();
+            Random mi = new Random();
 
-        else if (sighn == "/")
-        {
-            divide(a,b);
-        }
-        
-        else if (sighn == "+")
-        {
-            Add(a,b);
-        }
+           double y = fir.NextDouble();
+            x = fir.Next(1,4);
+          int z = mi.Next(1,10);
+            Console.WriteLine(z+x+y);
+            int fer = Convert.ToInt32(z - x);
+            Console.WriteLine(z);
+            Console.WriteLine(x);
+            Console.WriteLine(fer);
 
-        else if(sighn == "-")
-        {
-            subtract(a,b);
         }
-    }
-
-    static void Main()
-    {
-        Console.WriteLine("enter first number");
-        int a = Convert.ToInt32(Console.ReadLine());
-        
-        Console.WriteLine("enter second number number");
-        int b = Convert.ToInt32(Console.ReadLine());
-        
-        Console.WriteLine("enter a sighn");
-        string sighn  = Console.ReadLine();
-        
-        
-       calculator(a,sighn,b);
-
-      
     }
 }
